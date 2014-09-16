377d81a67762727dd3112f94377179962259c852
Bob Lounsbury <blounsbury@usbr.gov>
2014-09-16 13:22:29 -0600

    Added function to check constraints. John, the function had to have demand quantities passed to it. I'm not sure I passed the correct demand quantities to all the ObjectiveFunction calls. Also, the 0.9999 seems to produce a better result as compared to the Excel solution.

--- Hydro$ense/TracySolver.cs
+++ Hydro$ense/TracySolver.cs
@@ -13,7 +13,6 @@ namespace HydroSense
             int iter = 0;
             int maxIter = 5000;
             double delta = 1000;
-            double dampen = 1;
             double deltad = 0.01;
             double tolerance = 0.015;
             int numDemandNodes = mi.Q.Length;
@@ -26,7 +25,6 @@ namespace HydroSense
             double[][] delQ12 = Util.CopyArray(mi.Q);
             double[] delOFdelQ = new double[numNodes];
             double[] delOFdelQ2 = new double[numNodes];
-            double[] dQ = new double[numNodes];
             double[][] dQW = Util.CopyArray(mi.Q);
             double[][] del2OFdelQ2 = new double[numNodes][];
             Util.InitializeArrayToZero(del2OFdelQ2, numNodes, numNodes);
@@ -41,7 +39,7 @@ namespace HydroSense
                 }
             }
             Console.WriteLine("Initial Guess:");
-            double OF = ObjectiveFunction(mi, quantS);
+            double OF = ObjectiveFunction(mi, quantS, quantD);
             Util.PrintResultsToConsole(OF, quantS, quantD, iter, delta);
 
             /* Perform iterative colculations using a while loop construction
@@ -49,10 +47,18 @@ namespace HydroSense
              * Current routines use a simplified first derivative search
              * approach, with a dampening factor to adjust the decision variables
              * during each iteration */
+            double OF1;
+            double OF12;
+            double OF2;
             while (delta > tolerance && iter < maxIter)
             {
                 iter++;
-                OF = ObjectiveFunction(mi, quantS);
+                OF = ObjectiveFunction(mi, quantS, quantD);
+                
+                delQ1 = Util.CopyArray(quantS);
+                delQ2 = Util.CopyArray(quantS);
+                delQ12 = Util.CopyArray(quantS);
+
                 /* Compute the numerical derivates for the Objective Function with
                  * respect to each decision variable:
                  *   The Objective Function is the integral of the marginal demand
@@ -60,21 +66,13 @@ namespace HydroSense
                  *   the integral of the marginal cost functions from zero to the Quantity
                  *   provided from supply node j to demand node i. The DVs are the array 
                  *   of quantity from supply node j to demand node i */
-                double OF1;
-                double OF12;
-                double OF2;
-
-                delQ1 = Util.CopyArray(quantS);
-                delQ2 = Util.CopyArray(quantS);
-                delQ12 = Util.CopyArray(quantS);
-
                 for (int i = 0; i < quantS.Length; i++)
                 {
                     for (int j = 0; j < quantS[i].Length; j++)
                     {
                         delQ1[i][j] -= deltad;
                         delQ12[i][j] -= deltad;
-                        OF1 = ObjectiveFunction(mi, delQ1);
+                        OF1 = ObjectiveFunction(mi, delQ1, quantD);
                         delOFdelQ[i * numSupplyNodes + j] = (OF - OF1) / deltad;
 
                         for (int ki = 0; ki < i; ki++)
@@ -83,8 +81,8 @@ namespace HydroSense
                             {
                                 delQ2[ki][kj] -= deltad;
                                 delQ12[ki][kj] -= deltad;
-                                OF2 = ObjectiveFunction(mi, delQ2);
-                                OF12 = ObjectiveFunction(mi, delQ12);
+                                OF2 = ObjectiveFunction(mi, delQ2, quantD);
+                                OF12 = ObjectiveFunction(mi, delQ12, quantD);
                                 delOFdelQ2[ki * numSupplyNodes + kj] = (OF2 - OF12) / deltad;
                                 del2OFdelQ2[i * numSupplyNodes + j][ki * numSupplyNodes + kj] = (delOFdelQ[i * numSupplyNodes + j] - delOFdelQ2[ki * numSupplyNodes + kj]) / deltad;
                                 delQ2[ki][kj] = quantS[ki][kj];
@@ -96,8 +94,8 @@ namespace HydroSense
                                     continue;
                                 delQ2[ki][kj] -= deltad;
                                 delQ12[ki][kj] -= deltad;
-                                OF2 = ObjectiveFunction(mi, delQ2);
-                                OF12 = ObjectiveFunction(mi, delQ12);
+                                OF2 = ObjectiveFunction(mi, delQ2, quantD);
+                                OF12 = ObjectiveFunction(mi, delQ12, quantD);
                                 delOFdelQ2[ki * numSupplyNodes + kj] = (OF2 - OF12) / deltad;
                                 del2OFdelQ2[i * numSupplyNodes + j][ki * numSupplyNodes + kj] = (delOFdelQ[i * numSupplyNodes + j] - delOFdelQ2[ki * numSupplyNodes + kj]) / deltad;
                                 delQ2[ki][kj] = quantS[ki][kj];
@@ -110,8 +108,8 @@ namespace HydroSense
                             {
                                 delQ2[ki][kj] -= deltad;
                                 delQ12[ki][kj] -= deltad;
-                                OF2 = ObjectiveFunction(mi, delQ2);
-                                OF12 = ObjectiveFunction(mi, delQ12);
+                                OF2 = ObjectiveFunction(mi, delQ2, quantD);
+                                OF12 = ObjectiveFunction(mi, delQ12, quantD);
                                 delOFdelQ2[ki * numSupplyNodes + kj] = (OF2 - OF12) / deltad;
                                 del2OFdelQ2[i * numSupplyNodes + j][ki * numSupplyNodes + kj] = (delOFdelQ[i * numSupplyNodes + j] - delOFdelQ2[ki * numSupplyNodes + kj]) / deltad;
                                 delQ2[ki][kj] = quantS[ki][kj];
@@ -123,8 +121,8 @@ namespace HydroSense
                                     continue;
                                 delQ2[ki][kj] -= deltad;
                                 delQ12[ki][kj] -= deltad;
-                                OF2 = ObjectiveFunction(mi, delQ2);
-                                OF12 = ObjectiveFunction(mi, delQ12);
+                                OF2 = ObjectiveFunction(mi, delQ2, quantD);
+                                OF12 = ObjectiveFunction(mi, delQ12, quantD);
                                 delOFdelQ2[ki * numSupplyNodes + kj] = (OF2 - OF12) / deltad;
                                 del2OFdelQ2[i * numSupplyNodes + j][ki * numSupplyNodes + kj] = (delOFdelQ[i * numSupplyNodes + j] - delOFdelQ2[ki * numSupplyNodes + kj]) / deltad;
                                 delQ2[ki][kj] = quantS[ki][kj];
@@ -134,7 +132,7 @@ namespace HydroSense
 
                         /* set the second derivative on the diagonal */
                         delQ1[i][j] += 2 * deltad;
-                        OF2 = ObjectiveFunction(mi, delQ1);
+                        OF2 = ObjectiveFunction(mi, delQ1, quantD);
                         delOFdelQ2[i * numSupplyNodes + j] = (OF2 - OF) / deltad;
                         del2OFdelQ2[i * numSupplyNodes + j][i * numSupplyNodes + j] = (delOFdelQ2[i * numSupplyNodes + j] - delOFdelQ[i * numSupplyNodes + j]) / deltad;
 
@@ -150,29 +148,7 @@ namespace HydroSense
                 /*  Determine the change in quantity from Demand Node i to Supply Node j 
                  *  by solving the linear set of equations [d2OFdQ2]{dQ} = {dOFdQ}
                  *  using the LinearEquationSolver Routine */
-                dQ = LinearEquationSolver(numNodes, del2OFdelQ2, delOFdelQ);
-
-                /* The partial derivates of the Objective Function with respect to the
-                 * Quantity provided from supply node j to demand node i are the
-                 * "direction" that decision variables should be adjusted for the simple
-                 * non-linear solver routine.  The "distance" the decision variables should
-                 * be adjusted is assumed to be 1.0.  If the convergence metric (Delta)
-                 * increases from one iteration to the next, a dampening coefficient will
-                 * be used to adjust the "distance" to accelerate convergence
-                 * In addition, the current routine does not place bounds on the decision
-                 * variables.  Routines need to be added to prevent the QuantS from containing
-                 * negative values.  Additional routines could be put in place to bound
-                 * QuantS with maximum values also. */
-                double sum = 0.0;
-                for (int i = 0; i < dQ.Length; i++)
-                {
-                    sum += dQ[i] * dQ[i];
-                }
-                double delta1 = Math.Sqrt(sum);
-                //if (delta1 > delta)
-                //{
-                //    dampen *= delta / delta1;
-                //}
+                double[] dQ = LinearEquationSolver(numNodes, del2OFdelQ2, delOFdelQ);
                 for (int i = 0; i < quantS.Length; i++)
                 {
                     for (int j = 0; j < quantS[i].Length; j++)
@@ -182,87 +158,26 @@ namespace HydroSense
                     }
                 }
 
-                /* Determine the Quantity delivered to the Demand nodes by adjusting
-                 * for transportation losses from the supply nodes. */
-                for (int i = 0; i < quantD.Length; i++)
+                /* The partial derivates of the Objective Function with respect to the
+                 * Quantity provided from supply node j to demand node i are the
+                 * "direction" that decision variables should be adjusted for the simple
+                 * non-linear solver routine.  The "distance" the decision variables should
+                 * be adjusted is assumed to be 1.0. */
+                double sum = 0.0;
+                for (int i = 0; i < dQ.Length; i++)
                 {
-                    for (int j = 0; j < quantD[i].Length; j++)
-                    {
-                        quantD[i][j] = mi.linkLosses.LinkCostOrLoss(i, j, quantS[i][j]);
-                    }
+                    sum += dQ[i] * dQ[i];
                 }
-
-                /* Constrain Supply to Demand flows to be positive and not greater than
-                 * maximum quantity between Supply node j and Demand node i. */
-                for (int i = 0; i < quantS.Length; i++)
-                {
-                    for (int j = 0; j < quantS[i].Length; j++)
-                    {
-                        if (quantS[i][j] < mi.linkLosses.LowerLimit(i, j))
-                        {
-                            quantS[i][j] = mi.linkLosses.LowerLimit(i, j);
-                        }
-                        if (quantS[i][j] > mi.linkLosses.UpperLimit(i, j))
-                        {
-                            quantS[i][j] = mi.linkLosses.UpperLimit(i, j);
-                        }
-                    }
-                }
-
-                for (int i = 0; i < quantS.Length; i++)
-                {
-                    for (int j = 0; j < quantS[i].Length; j++)
-                    {
-                        quantD[i][j] = mi.linkLosses.LinkCostOrLoss(i, j, quantS[i][j]);
-                    }
-                }
-
-                /* Determine if Demand Node Constraints are violated
-                 * If they are, redistribute flows proportionally from Supplies */
-                for (int i = 0; i < quantD.Length; i++)
-                {
-                    double totalD = Util.SumRow(quantD, i);
-                    double limit = mi.demandNodes.UpperLimit(i);
-                    if (totalD > limit)
-                    {
-                        double ratio = limit / totalD;
-                        for (int j = 0; j < quantD[i].Length; j++)
-                        {
-                            quantD[i][j] *= ratio;
-                        }
-                    }
-                }
-
-                /* Adjust the Supply Quantities to match the adjusted Demand Quantities */
-                for (int i = 0; i < quantS.Length; i++)
-                {
-                    for (int j = 0; j < quantS[i].Length; j++)
-                    {
-                        quantS[i][j] = mi.linkLosses.LossInverse(i, j, quantD[i][j]);
-                    }
-                }
-
-                /* Determine if Supply Node Constraints are violated
-                 * If they are, redistribute flow proportionally between Demand Nodes */
-                for (int i = 0; i < quantS[0].Length; i++)
-                {
-                    double totalS = Util.SumColumn(quantS, i);
-                    double limit = mi.supplyNodes.UpperLimit(i);
-                    if (totalS > limit)
-                    {
-                        double ratio = 0.9999 * (limit / totalS);
-                        for (int j = 0; j < quantS.Length; j++)
-                        {
-                            quantS[j][i] *= ratio;
-                        }
-                    }
-                }
-
-                /* Calculate new objective function */
-                OF = ObjectiveFunction(mi, quantS);
 
                 /* Adjust change in decision variables */
-                delta = delta1 * dampen;
+                delta = Math.Sqrt(sum);
+
+                /* Adjust quantities to make sure they are all within
+                 * the supply, demand, and transportation constraints */
+                CheckConstraints(mi, quantS, quantD);
+
+                /* Calculate new objective function */
+                OF = ObjectiveFunction(mi, quantS, quantD);
 
                 /* a little output to check against Python code */
                 Util.PrintResultsToConsole(OF, quantS, quantD, iter, delta);
@@ -270,8 +185,97 @@ namespace HydroSense
             mi.Q = quantS;
         }
 
+        /// <summary>
+        /// Adjust quantities to make sure they are all within the 
+        /// supply, demand, and transportation constraints
+        /// </summary>
+        /// <param name="mi">Model input data</param>
+        /// <param name="quantS">Supply quantities</param>
+        /// <param name="quantD">Demand quantities</param>
+        private void CheckConstraints(ModelInput mi, double[][] quantS, double[][] quantD)
+        {
+            /* Determine the Quantity delivered to the Demand nodes by adjusting
+             * for transportation losses from the supply nodes. */
+            for (int i = 0; i < quantD.Length; i++)
+            {
+                for (int j = 0; j < quantD[i].Length; j++)
+                {
+                    quantD[i][j] = mi.linkLosses.LinkCostOrLoss(i, j, quantS[i][j]);
+                }
+            }
 
-        private double ObjectiveFunction(ModelInput m, double[][] quantities)
+            /* Constrain Supply to Demand flows to be positive and not greater than
+             * maximum quantity between Supply node j and Demand node i. */
+            for (int i = 0; i < quantS.Length; i++)
+            {
+                for (int j = 0; j < quantS[i].Length; j++)
+                {
+                    if (quantS[i][j] < mi.linkLosses.LowerLimit(i, j))
+                    {
+                        quantS[i][j] = mi.linkLosses.LowerLimit(i, j);
+                    }
+                    if (quantS[i][j] > mi.linkLosses.UpperLimit(i, j))
+                    {
+                        quantS[i][j] = mi.linkLosses.UpperLimit(i, j);
+                    }
+                }
+            }
+
+            /* Determine the Quantity delivered to the Demand nodes by adjusting
+             * for transportation losses from the supply nodes after constraining
+             * supply flows */
+            for (int i = 0; i < quantS.Length; i++)
+            {
+                for (int j = 0; j < quantS[i].Length; j++)
+                {
+                    quantD[i][j] = mi.linkLosses.LinkCostOrLoss(i, j, quantS[i][j]);
+                }
+            }
+
+            /* Determine if Demand Node Constraints are violated
+             * If they are, redistribute flows proportionally from Supplies */
+            for (int i = 0; i < quantD.Length; i++)
+            {
+                double totalD = Util.SumRow(quantD, i);
+                double limit = mi.demandNodes.UpperLimit(i);
+                if (totalD > limit)
+                {
+                    double ratio = limit / totalD;
+                    for (int j = 0; j < quantD[i].Length; j++)
+                    {
+                        quantD[i][j] *= ratio;
+                    }
+                }
+            }
+
+            /* Adjust the Supply Quantities to match the adjusted Demand Quantities */
+            for (int i = 0; i < quantS.Length; i++)
+            {
+                for (int j = 0; j < quantS[i].Length; j++)
+                {
+                    quantS[i][j] = mi.linkLosses.LossInverse(i, j, quantD[i][j]);
+                }
+            }
+
+            /* Determine if Supply Node Constraints are violated
+             * If they are, redistribute flow proportionally between Demand Nodes */
+            for (int i = 0; i < quantS[0].Length; i++)
+            {
+                double totalS = Util.SumColumn(quantS, i);
+                double limit = mi.supplyNodes.UpperLimit(i);
+                if (totalS > limit)
+                {
+                    double ratio = 0.9999 * (limit / totalS);
+                    for (int j = 0; j < quantS.Length; j++)
+                    {
+                        quantS[j][i] *= ratio;
+                    }
+                }
+            }
+        }
+
+
+        private double ObjectiveFunction(ModelInput m, double[][] quantS, double[][] quantD)
         {
             /* The ObjectiveFunction is computed by integrating the sum of the
              * difference between the Demand Functions and the Supply Functions.
@@ -285,36 +289,27 @@ namespace HydroSense
              * be defined as global variables, so that only the Qs values (DVs) would
              * have to be passed to the routine. */
             double rval = 0.0;
-            /*
-             * Adjust quantities to make sure they are all within
-             * the supply, demand and transportation constraints
-             */
-            for (int i = 0; i < quantities.Length; i++)
-            {
-                for (int j = 0; j < quantities[0].Length; j++)
-                {
-                    if (quantities[i][j] < m.linkLosses.LowerLimit(i, j))
-                    {
-                        quantities[i][j] = m.linkLosses.LowerLimit(i, j);
-                    }
-                }
-            }
+
+            /* Adjust quantities to make sure they are all within
+             * the supply, demand, and transportation constraints */
+            CheckConstraints(m, quantS, quantD);
+
             /* Calculate total cost of water for each supply node */
-            for (int i = 0; i < quantities[0].Length; i++)
+            for (int i = 0; i < quantS[0].Length; i++)
             {
-                rval -= m.supplyNodes.IntegratedCost(i, Util.SumColumn(quantities, i));
+                rval -= m.supplyNodes.IntegratedCost(i, Util.SumColumn(quantS, i));
             }
 
             /* Calculate total benefits of water for each demand node and the
              * transportation/link cost associated with delivering water from
              * Supply node j to Demand node i */
-            for (int i = 0; i < quantities.Length; i++)
+            for (int i = 0; i < quantS.Length; i++)
             {
                 double qd = 0.0;
-                for (int j = 0; j < quantities[i].Length; j++)
+                for (int j = 0; j < quantS[i].Length; j++)
                 {
-                    qd += m.linkLosses.LinkCostOrLoss(i, j, quantities[i][j]);
-                    rval -= m.linkCosts.IntegratedCost(i, j, quantities[i][j]);
+                    qd += m.linkLosses.LinkCostOrLoss(i, j, quantS[i][j]);
+                    rval -= m.linkCosts.IntegratedCost(i, j, quantS[i][j]);
                 }
                 rval += m.demandNodes.IntegratedCost(i, qd);
             }
